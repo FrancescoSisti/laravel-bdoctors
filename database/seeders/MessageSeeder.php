9b7a87498c74079d3948ac2a8330c6ff15ea2db5
FrancescoSisti <francescosisti61@gmail.com>
2024-12-03 17:40:26 +0100

    Add MessageSeeder for populating messages in the database and minor formatting adjustments in API routes

---
+++ database/seeders/MessageSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Message;
+use App\Models\Profile;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class MessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $profileIds = Profile::all()->pluck("id");
+
+        foreach($profileIds as $profileId) {
+            for($i = 0; $i < ($faker->numberBetween(1,3)); $i++) {
+                $newMessage = new Message();
+                $newMessage->profile_id = $profileId;
+                $newMessage->content = $faker->realText(rand(50,200));
+                $newMessage->email = $faker->email();
+                $newMessage->first_name = $faker->firstName();
+                $newMessage->last_name = $faker->lastName();
+                $newMessage->save();
+            }
+        }
+    }
+}
