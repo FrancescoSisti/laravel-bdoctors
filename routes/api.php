bc9b9732c6b21d039963ef95c907766671de823b
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-12-05 12:43:13 +0100

    Bug fixed

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::get('/specializations', function () {
 // Profile routes
 Route::get('/profiles/{id}', [ShowController::class, 'show'])->name('api.profiles.show');
 Route::post('/profiles', [CreateController::class, 'create'])->name('api.profiles.create');
-Route::get('/profiles/edit/{id}', [EditController::class. 'edit'])->name('api.profiles.edit');
+Route::get('/profiles/edit/{id}', [EditController::class, 'edit'])->name('api.profiles.edit');
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
