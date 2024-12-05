f7fdd25cfa6d5105371d5b8c054a730a56ec66da
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-12-05 15:29:03 +0100

    Add the UpdateContoller for resource Profile. Write the api route for the method Update of the same controller.

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\ShowController;
 use App\Http\Controllers\Api\CreateController;
 use App\Http\Controllers\Api\EditController;
 use App\Http\Controllers\Api\RegisterController as ApiRegisterController;
+use App\Http\Controllers\Api\UpdateController;
 use App\Http\Controllers\Auth\RegisterController as AuthRegisterController;
 use App\Models\Specialization;
 
@@ -38,6 +39,7 @@ Route::get('/specializations', function () {
 Route::get('/profiles/{id}', [ShowController::class, 'show'])->name('api.profiles.show');
 Route::post('/profiles', [CreateController::class, 'create'])->name('api.profiles.create');
 Route::get('/profiles/edit/{id}', [EditController::class, 'edit'])->name('api.profiles.edit');
+Route::post('/profiles/edit/{id}', [UpdateController::class, 'update'])->name('api.profiles.update');
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
