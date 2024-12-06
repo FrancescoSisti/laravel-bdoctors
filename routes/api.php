8714c8aaf7105e717da2d0ed357c1e3643c4c854
FrancescoSisti <francescosisti61@gmail.com>
2024-12-06 09:55:36 +0100

    Refactor API routes to use LoginController for login and add update route for profiles
    - Updated the login route to utilize LoginController for improved authentication handling.
    - Added a new PUT route for updating profiles, linking it to the EditController's update method.
    - Ensured consistent organization of API routes by maintaining the use of the 'App\Http\Controllers\Api' namespace.

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\ShowController;
 use App\Http\Controllers\Api\CreateController;
 use App\Http\Controllers\Api\EditController;
+use App\Http\Controllers\Api\LoginController;
 use App\Models\Specialization;
 
 /*
@@ -21,7 +22,7 @@ use App\Models\Specialization;
 */
 
 // Public routes
-Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::post('/login', [LoginController::class, 'login'])->name('api.login');
 Route::post('/register', [RegisterController::class, 'register'])->name('api.register');
 
 // Specializations route
@@ -36,6 +37,7 @@ Route::get('/specializations', function () {
 Route::get('/profiles/{id}', [ShowController::class, 'show'])->name('api.profiles.show');
 Route::post('/profiles', [CreateController::class, 'create'])->name('api.profiles.create');
 Route::get('/profiles/edit/{id}', [EditController::class, 'edit'])->name('api.profiles.edit');
+Route::put('/profiles/{id}', [EditController::class, 'update'])->name('api.profiles.update');
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
