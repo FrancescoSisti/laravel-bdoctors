0a343870b545d5b60e671f7f5804f2a0b3b099bb
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-13 11:07:45 +0100

    Added average votes query

--- app/Http/Controllers/Api/FilteredSearchController.php
+++ app/Http/Controllers/Api/FilteredSearchController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use App\Models\Review;
 use Illuminate\Http\Request;
 
 class FilteredSearchController extends Controller
 {
-        public function filter() {
+    public function filter(Request $request)
+    {
 
-            $reviews = Review::with(['profiles'])->get();
-            //dd($reviews);
-            return response()->json([
-                'success' => true,
-                'reviews' => $reviews
-        ]);
+        $min_average_votes = $request->query('min_average_votes');
+
+        $query = Profile::select('profiles.*')
+            ->leftJoin('reviews', 'profiles.id', '=', 'reviews.profile_id')
+            ->selectRaw('AVG(reviews.votes) as average_votes')
+            ->groupBy('profiles.id');
+
+        if ($min_average_votes) {
+            $query->havingRaw('AVG(reviews.votes) >= 0', [$min_average_votes]);
         }
+
+        $profiles = $query->get();
+
+
+        return response()->json([
+            'success' => true,
+            'profiles' => $profiles
+        ]);
+    }
 }
