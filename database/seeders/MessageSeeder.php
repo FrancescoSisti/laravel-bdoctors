2b3807dd57fa32ef926b55b56a56f50c1f1bec2a
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-12-16 16:04:26 +0100

    Modified the seeder for message: now the created-at and update_at column are seeded with random data between 2 specific data.

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use App\Models\Message;
 use App\Models\Profile;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,16 +19,36 @@ class MessageSeeder extends Seeder
     {
         $profileIds = Profile::all()->pluck("id");
 
-        foreach($profileIds as $profileId) {
-            for($i = 0; $i < ($faker->numberBetween(1,3)); $i++) {
+        foreach ($profileIds as $profileId) {
+            for ($i = 0; $i < ($faker->numberBetween(20, 36)); $i++) {
                 $newMessage = new Message();
                 $newMessage->profile_id = $profileId;
-                $newMessage->content = $faker->realText(rand(50,200));
+                $newMessage->content = $faker->realText(rand(50, 200));
                 $newMessage->email = $faker->email();
                 $newMessage->first_name = $faker->firstName();
                 $newMessage->last_name = $faker->lastName();
+
+
+                $startDate = Carbon::create(2024, 1, 1, 0, 0, 0);
+                $endDate = Carbon::create(2024, 12, 31, 0, 0, 0);
+                $randomDate = $faker->dateTimeBetween($startDate, $endDate);
+                $newMessage->created_at = $randomDate;
+                $newMessage->updated_at = $randomDate;
+
                 $newMessage->save();
             }
         }
+
+        // foreach($profileIds as $profileId) {
+        //     for($i = 0; $i < ($faker->numberBetween(1,3)); $i++) {
+        //         $newMessage = new Message();
+        //         $newMessage->profile_id = $profileId;
+        //         $newMessage->content = $faker->realText(rand(50,200));
+        //         $newMessage->email = $faker->email();
+        //         $newMessage->first_name = $faker->firstName();
+        //         $newMessage->last_name = $faker->lastName();
+        //         $newMessage->save();
+        //     }
+        // }
     }
 }
