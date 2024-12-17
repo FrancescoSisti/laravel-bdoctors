4296b0f98e7010dbe4b9dddb282c69f8fa01b92f
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-17 12:00:08 +0100

    removed sponsorship json data

--- app/Http/Controllers/Api/FilteredSearchController.php
+++ app/Http/Controllers/Api/FilteredSearchController.php
@@ -16,15 +16,16 @@ class FilteredSearchController extends Controller
         // $input_rating = $rating->query('input_rating');
 
         if ($id) {
-            $query = User::select('users.*', 'profiles.*', 'specializations.id as specializations_id', 'specializations.name as specializations_name', 'sponsorships.id as sponsorship_id')
+            $query = User::select('users.*', 'profiles.*', 'specializations.id as specializations_id', 'specializations.name as specializations_name',)
                 ->join('specialization_user', 'users.id', '=', 'specialization_user.user_id')
                 ->join('specializations', 'specializations.id', '=', 'specialization_user.specialization_id')
                 ->join('profiles', 'users.id', '=', 'profiles.user_id')
-                ->join('profile_sponsorship', 'profiles.id', '=', 'profile_sponsorship.profile_id')
-                ->join('sponsorships', 'sponsorships.id', '=', 'profile_sponsorship.sponsorship_id')
+                // sponsorship
+                // ->join('profile_sponsorship', 'profiles.id', '=', 'profile_sponsorship.profile_id')
+                // ->join('sponsorships', 'sponsorships.id', '=', 'profile_sponsorship.sponsorship_id')
                 ->leftJoin('reviews', 'profiles.id', '=', 'reviews.profile_id')
                 ->where('specialization_user.specialization_id', '=', $id)
-                ->groupBy('users.id', 'profiles.id', 'specializations.id', 'sponsorships.id');
+                ->groupBy('users.id', 'profiles.id', 'specializations.id');
         }
 
         $query->selectRaw(
